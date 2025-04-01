09ae2938ea305b394aa19638cf033bdc39899407
SohanKafle <soka_bca2079@lict.edu.np>
2025-04-01 13:47:32 +0545

    commit

--- elementslist.dart
+++ elementslist.dart
@@ -11,8 +11,6 @@
 
 void main() {
   List<int> a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
-  
-  // Filter and print elements less than 5
   for (int element in a) {
     if (element < 5) {
       print(element);
