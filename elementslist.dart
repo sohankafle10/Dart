dab16ba5e0fadce5332c1b2639661e00df0d5a14
SohanKafle <soka_bca2079@lict.edu.np>
2025-04-01 13:47:00 +0545

    commit

---
+++ elementslist.dart
@@ -0,0 +1,22 @@
+//take a list, say for example this one:
+// a={1,1,2,3,5,8,13,21,34,55,89} and
+// write a program that prints out all the elements of the list 
+//that are less than 5
+
+
+
+
+
+
+
+void main() {
+  List<int> a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+  
+  // Filter and print elements less than 5
+  for (int element in a) {
+    if (element < 5) {
+      print(element);
+    }
+  }
+}
+
